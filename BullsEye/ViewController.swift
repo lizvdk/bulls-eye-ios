d386ec5b49e8bb49d47655d4f08e9b8e6ad1f84d
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-11-28 11:35:21 -0500

    Calculate and show random target

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 0
+    var targetValue: Int = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +28,7 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let message = "The value of the slider is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(  title: "Hello, World",
                                         message: message,
@@ -33,10 +38,23 @@ class ViewController: UIViewController {
                                         handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+    
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 }
 
