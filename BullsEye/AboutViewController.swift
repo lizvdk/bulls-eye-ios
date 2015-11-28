c598aaea6c6888b9cce2489fa3f280951fa7750c
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-11-28 13:56:59 -0500

    Add about screen

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Liz Vanderkloot on 11/28/15.
+//  Copyright © 2015 lizvdk. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
